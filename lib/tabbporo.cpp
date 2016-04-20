057e1b836fe39868e638221ccde295f51c5c7164
Jorge Espinosa <sonip396@users.noreply.github.com>
2016-04-20 14:41:22 +0200

    upload

--- lib/tabbporo.cpp
+++ lib/tabbporo.cpp
@@ -7,18 +7,33 @@
 
 #include "../include/tabbporo.h"
 
-TNodoABB::TNodoABB() {
+TNodoABB::TNodoABB():iz(), de() {}
+
+TNodoABB::TNodoABB(TNodoABB &origen){
+	Copia(origen);
 }
 
-TNodoABB::TNodoABB(TNodoABB&) {
+TNodoABB::~TNodoABB() {} //Llama a los destructores de cada tipo de dato automáticamente
+
+void TNodoABB::Copia(const TNodoABB &origen){
+	(this)->item = origen.item;
+	if(!origen.de.EsVacio())
+		(this)->de = origen.de;
+	if(!origen.iz.EsVacio())
+		(this)->iz = origen.iz;
+}
+TNodoABB& TNodoABB::operator =(const TNodoABB &origen) {
+	if(this != &origen)
+	{
+		(this)->~TNodoABB();
+		Copia(origen);
+	}
+	return *this;
 }
 
-TNodoABB::~TNodoABB() {
-}
-
-TNodoABB& TNodoABB::operator =(TNodoABB&) {
-}
-
+/*
+ * ****************************************************************************
+ */
 void TABBPoro::InordenAux(TVectorPoro&, int&) {
 }
 
@@ -29,24 +44,52 @@ void TABBPoro::PostordenAux(TVectorPoro&, int&) {
 }
 
 TABBPoro::TABBPoro() {
+	nodo = NULL;
 }
 
-TABBPoro::TABBPoro(TABBPoro&) {
+TABBPoro::TABBPoro(TABBPoro &) {
 }
 
 TABBPoro::~TABBPoro() {
 }
 
-TABBPoro& TABBPoro::operator =(TABBPoro&) {
+TABBPoro& TABBPoro::operator =(const TABBPoro &origen) {
 }
 
 bool TABBPoro::operator ==(TABBPoro&) {
 }
 
-bool TABBPoro::EsVacio() {
+bool TABBPoro::EsVacio() const{
+	return nodo == NULL;
 }
 
-bool TABBPoro::Insertar(TPoro&) {
+bool TABBPoro::FiltraVolumen(int volumen) {
+	return true;
+}
+
+void TABBPoro::BuscaInserta(TNodoABB &nuevo){
+	//Comprobar recursivamente que sea menor o mayor que el elemento nodo de la raiz e introducir donde quepa
+	if(nuevo.item.Volumen() < nodo->item.Volumen())
+	{
+
+	}
+}
+
+bool TABBPoro::Insertar(const TPoro &poro) {
+	if(FiltraVolumen(poro.Volumen()))
+	{
+		//se crea el nodo a insertar
+		TNodoABB *aux = new TNodoABB();
+		aux->item = poro;
+		//se busca e inserta el nodo
+		if(EsVacio())
+			nodo = aux;
+		else
+			BuscaInserta(*aux);
+
+		return true;
+	}
+	return false;
 }
 
 bool TABBPoro::Borrar(TPoro&) {
@@ -68,12 +111,30 @@ int TABBPoro::NodosHoja() {
 }
 
 TVectorPoro TABBPoro::Inorden() {
+	//Posición en el vector que almacena el recorrido
+	int posicion = 1;
+	//Vector del tamaño adecuado para almacenar todos los nodos
+	TVectorPoro v(Nodos());
+	InordenAux(v, posicion);
+	return v;
 }
 
 TVectorPoro TABBPoro::Preorden() {
+	//Posición en el vector que almacena el recorrido
+	int posicion = 1;
+	//Vector del tamaño adecuado para almacenar todos los nodos
+	TVectorPoro v(Nodos());
+	PreordenAux(v, posicion);
+	return v;
 }
 
 TVectorPoro TABBPoro::Postorden() {
+	//Posición en el vector que almacena el recorrido
+	int posicion = 1;
+	//Vector del tamaño adecuado para almacenar todos los nodos
+	TVectorPoro v(Nodos());
+	PostordenAux(v, posicion);
+	return v;
 }
 
 TVectorPoro TABBPoro::Niveles() {
